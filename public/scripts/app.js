b5c9c8f0a8d64377f7228937871d9b5b21beb303
tsudodog <malamute29@gmail.com>
2018-09-18 18:37:03 -0500

    finished a few more lessons

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,12 +1,71 @@
-'use strict';
+"use strict";
 
 console.log('App.js is running!');
 
 //JSX Java Script XML, it is a javascript extension
+var appObject = {
+    title: "hello",
+    subTitle: "World"
+};
+
 var template = React.createElement(
-  'h1',
-  null,
-  'This is JSX2 from app.js'
+    "div",
+    null,
+    React.createElement(
+        "h1",
+        null,
+        appObject.title
+    ),
+    React.createElement(
+        "p",
+        null,
+        appObject.subTitle
+    ),
+    React.createElement(
+        "ol",
+        null,
+        React.createElement(
+            "li",
+            null,
+            "Item one"
+        ),
+        React.createElement(
+            "li",
+            null,
+            "Item two"
+        )
+    )
+);
+
+//Create a templateTwo Variable
+var user = {
+    userName: 'TsudoDog',
+    userAge: 27,
+    userLocation: 'Saint Paul'
+};
+var userName = 'TsudoDog';
+var userAge = 27;
+var userLocation = 'Saint Paul';
+var templateTwo = React.createElement(
+    "div",
+    null,
+    React.createElement(
+        "h1",
+        null,
+        user.userName.toUpperCase()
+    ),
+    React.createElement(
+        "p",
+        null,
+        "Age: ",
+        userAge
+    ),
+    React.createElement(
+        "p",
+        null,
+        "Location: ",
+        userLocation
+    )
 );
 
 var appRoot = document.getElementById('app');
--- src/app.js
+++ src/app.js
@@ -1,7 +1,40 @@
 console.log('App.js is running!');
 
 //JSX Java Script XML, it is a javascript extension
-var template = <h1>This is JSX2 from app.js</h1>;
+var appObject = {
+    title: "hello",
+    subTitle: "World"
+}
+
+var template = (
+    <div>
+        <h1>{appObject.title}</h1>
+        <p>{appObject.subTitle}</p>
+        <ol>
+            <li>Item one</li>
+            <li>Item two</li>
+        </ol>
+    </div>
+);
+
+//Create a templateTwo Variable
+var user = {
+    userName : 'TsudoDog',
+    userAge : 27,
+    userLocation : 'Saint Paul'
+}
+var userName = 'TsudoDog';
+var userAge = 27;
+var userLocation = 'Saint Paul';
+var templateTwo = (
+    <div>
+        <h1>{user.userName.toUpperCase()}</h1>
+        <p>Age: {userAge}</p>
+        <p>Location: {userLocation}</p>
+
+    </div>
+);
+
 
 var appRoot = document.getElementById('app');
 
