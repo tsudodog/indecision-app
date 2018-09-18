35d34bb63edd9326b5c6cc4f1cd9a547e0266545
tsudodog <malamute29@gmail.com>
2018-09-18 00:46:27 -0500

    added the compiled code passed to babel:

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,7 +1,7 @@
 console.log('App.js is running!');
 
 //JSX Java Script XML, it is a javascript extension
-
+//var template = <p>This is JSX from app.js</p>;
 var template = React.createElement(
     "p",
     null,
